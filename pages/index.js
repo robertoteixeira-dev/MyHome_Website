8821ceef1954dadda6a87eee946f79e5f29a644c
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-04-23 23:33:03 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+
+const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, LinkName, imageUrl }) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5" >
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+      <Text fontSize="3x1" fontWeight="bold">{title1}<br />{title2}</Text>
+      <Text fontSize="lg" padingTop="3" paddingBottom="3" color="gray.700">{desc1}<br />{desc2}</Text>
+      <Button fontSize="xl" bg="blue.300" color="white">
+        <Link href={LinkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+)
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Banner
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        LinkName="/search?purpose=for-rent"
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
+
+      <Banner
+        purpose={'For Rent'}
+      />
+    </div>
+  )
+}
